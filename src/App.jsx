85c489dbdb3a1a1d07d420eb60ba118ea7200602
RizziFrancesco <rizzi.francesco@panettipitagora.edu.it>
2023-03-11 11:57:11 +0100

    Aggiunta varianti e callback
    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import Button from './Components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
-
   const [joke, setJoke] = useState("")
 
+  let loadJokeCallback = function(){
+    console.log("ciao")
+    setJoke("testo")
+  }
+
+  let copyTextCallback = function(){
+    console.log("bye")
+
+  }
   return (
     <div className='App'>
       <h1>Benvenuti</h1>
@@ -17,11 +25,8 @@ function App() {
         <button onClick={() => setCount((count) => count+1)}>
           count is {count}
         </button>
-
-    <Button text="carica il joke"></Button>
-    <Button text="copia"></Button>
-
-
+    <Button text="carica il joke" callback={loadJokeCallback}></Button>
+    <Button text="copia" variant={joke === "" ? "disabled": undefined }callback={copyTextCallback}></Button>
       </div>
     </div>
   )
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,12 +1,30 @@
 import {userState} from 'react'
 import '../Styles/Button.css'
 
-function Button({ text }){
+function Button({ text, variant, callback }){      
+        const[count, setCount] = useState(0)
+        let clickHandler = function(e){
+            console.log(e)
+            if(variant !="disabled"){
+                callback()
+            }          
+        }
+
+
+    let classRenderer = function(){
+        let classes = [
+          'Button', variant
+        ]
+        return classes.join(" ")
+    }
+    
     return(
-        <div className='Button'>
+        <div onClick={clickHandler} className={classRenderer()}>
             { text }
         </div>
     )
 }
+
+
 
 export default Button
