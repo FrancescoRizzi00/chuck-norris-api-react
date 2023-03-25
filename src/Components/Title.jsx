6db7163deb8af604e09e051f9df00f90001ffbb8
RizziFrancesco <rizzi.francesco@panettipitagora.edu.it>
2023-03-25 11:51:24 +0100

    Aggiunte

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import '../Styles/Title.css'
 
-function App() {
+function Title() {
   const [count, setCount] = useState(0)
 
   return (
@@ -12,4 +12,4 @@ function App() {
   )
 }
 
-export default App
+export default Title
